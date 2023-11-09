c66b5cfbe54939f0b0fa74f8f116fe579d1a5d0c
mwiin561 <mwiineirwin23@gmail.com>
2023-11-09 17:51:11 +0300

    trying stuff in the lab
    rendering the text "hello world" as well as using the useState() hook

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,3 +1,4 @@
+// well what this basically does is render the text "hello world"
 import React from 'react';
 
 const MyComponent = () => {
@@ -9,3 +10,30 @@ const MyComponent = () => {
 };
 
 export default MyComponent;
+
+//this is another snippet that will use the useState() hook
+import React, { useState } from 'react';
+
+const CounterComponent = () => {
+  const [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  function handleDecrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default CounterComponent;
+
+//
