f5950b04083b7e60725729f86a0ae9786f641e60
mwiin561 <mwiineirwin23@gmail.com>
2023-11-09 17:58:57 +0300

    use state () hook
    yyaaawwnnn

---
+++ src/MyComponent2.js
@@ -0,0 +1,24 @@
+//this is another snippet that will use the useState() hook
+import React, { useState } from 'react';
+
+const CounterComponent = () => {
+  const [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  function handleDecrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default CounterComponent;
